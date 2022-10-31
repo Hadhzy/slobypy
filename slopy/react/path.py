699354ddb633c8d89fcac5c249e7cdb0aa75d532
Hadházy Tamás <helsgithub15@gmail.com>
2022-10-31 14:13:13 +0100

    Remove events file

--- slopy/react/path.py
+++ slopy/react/path.py
@@ -1,12 +1,13 @@
 
 class Path:
-    PATH = []
+    PATHS = []
+
     def __init__(self, path) -> None:
         self.path = path
 
     @classmethod
     def add_path(cls, path):
-        cls.PATH.append(path)
+        cls.PATHS.append(path)
 
     def __call__(self, component):
         self.add_path({component: self.path})
