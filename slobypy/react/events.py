d076742fff29c402f6a3b6b454904c1f26e50d79
Hadházy Tamás <helsgithub15@gmail.com>
2022-11-04 13:51:46 +0100

    few changes

---
+++ slobypy/react/events.py
@@ -0,0 +1,17 @@
+from slobypy.rpc import Event
+
+
+class EventHandler:
+    """
+        description needed
+
+    """
+    @staticmethod
+    def listen(event: Event = None):
+        def listen_decorator(func):
+            def wrapper(*args, **kwargs):
+                result = func(*args, **kwargs)
+                print(event)
+                return result
+            return wrapper
+        return listen_decorator
