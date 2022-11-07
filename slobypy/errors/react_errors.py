13b9247666499b92c51733b4eb0abebe19da7d26
Hadházy Tamás <helsgithub15@gmail.com>
2022-11-07 13:01:19 +0100

    Create react error for uri

---
+++ slobypy/errors/react_errors.py
@@ -0,0 +1,4 @@
+class URI_ERROR(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
--- slobypy/react/tools.py
+++ slobypy/react/tools.py
@@ -1,6 +1,6 @@
 # This Project
 from slobypy.react._types import url_type
-
+from slobypy.errors.react_errors import URI_ERROR
 # Third-Party
 from urllib.parse import urlparse
 
@@ -20,4 +20,4 @@ def url_checker(url: url_type) -> str | bool:
     if slobypy_result.path and slobypy_result.scheme is not True:
         return url
 
-    return False
+    raise URI_ERROR("Not valid url")
