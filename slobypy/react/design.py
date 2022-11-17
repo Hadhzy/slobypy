2922d7bcd9eb93e90d160dbcc1a9b3b364b06e51
Hadházy Tamás <helsgithub15@gmail.com>
2022-11-17 18:50:12 +0100

    Added design class

---
+++ slobypy/react/design.py
@@ -0,0 +1,26 @@
+
+
+class Design:
+    """
+
+    The Design class is used to register the scss classes.
+    ### Arguments
+    None
+    ### Returns
+    list: the scss_class system styles(every single scss class style)
+
+    """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- slobypy/react/scss_classes.py
+++ slobypy/react/scss_classes.py
@@ -17,19 +17,64 @@ class SCSS_CLASS:
 
         for key, value in kwargs.items():
             self.STYLE_CLASS.__setattr__(key, value)
+
             self._style_data.append({key: value})  # update local style data
-            self.add_style(key, value)  # update global style data
+            self.add_style_global(key=key, value=value)  # update global style data
+
+    def add_class_style(self, *dict: dict | list[dict], key: str = "", value: str = "") -> None:
+        """
+      This method is used to add the style to the local class.
+      ### Arguments
+      - key: the key of the dictionary
+      - value: the value of the dictionary
+      - *dict: a single dict or a list of dicts
+      ### Returns
+      None
+        """
+        if dict:
+            self._style_data.append(dict)
+            self.add_style_global(dict)
+        else:
+            self._style_data.append({key: value})
+            self.add_style_global(key=value, value=value)
+
 
     @classmethod
-    def add_style(cls, key: str, value: str):
-        cls._STYLES.append({key: value})
+    def add_style_global(cls, *dict: dict | list[dict] | tuple, key: str = "", value: str = "") -> None:
+        """
+        This method is used to add the style to the class attributes(class system) !not to the instance.
+        ### Arguments
+        - key: the key of the dictionary
+        - value: the value of the dictionary
+        - *dict: a single dict or a list of dicts
+        ### Returns
+        None
+        """
+        if dict:
+            cls._STYLES.append(dict)
+        else:
+            cls._STYLES.append({key: value})
 
     @property
     def style_data(self) -> list:
+        """
+        This method is used to return the styles as a list.
+        ### Arguments
+        None
+        ### Returns
+        list: the scss class local styles
+        """
         return self._style_data
 
     @classmethod
     def get_styles(cls) -> list:
+        """
+        This method is used to return the scss_class system styles as a list(all of them).
+        ### Arguments
+        None
+        ### Returns
+        list: the scss_class system styles(every single scss class style)
+         """
         return cls._STYLES
 
     def __str__(self) -> str:
