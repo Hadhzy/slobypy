f5ec3bc75df8fcfca98ea91ad316a77277668652
Hadházy Tamás <helsgithub15@gmail.com>
2022-11-17 13:46:53 +0100

    Added test

--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,6 @@ from slobypy.react.scss_classes import SCSS_CLASS
 from slobypy.react import *
 import inspect
 
-# {"base_class": {"child1"}}
 my_class = SCSS_CLASS(
     name="base_class",
     position="relative",
