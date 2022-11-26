f404810d0177731793c458655cb9c8f4dd730830
Hadházy Tamás <helsgithub15@gmail.com>
2022-11-26 18:30:25 +0100

    Extend data builder

--- slobypy/communication/data_builder.py
+++ slobypy/communication/data_builder.py
@@ -3,21 +3,29 @@ from __future__ import annotations
 from slobypy.react import Design
 # Built-in
 import json
-
+from typing import Self
 
 
 class DataBuilder:
     """
     This class is build the data for the frontend.
     """
+    __JSON_DATA = ""  # Todo: Type hint here
 
-    def make_scss_data(self) -> None:
+    def make_scss_data(self) -> Self:
         """
         This method is used to create the scss data.
         """
-        pass
 
-    def make_app_component_data(self, components_data: str) -> None:
+        for scss_group in Design.get_registered_groups():
+            self.__JSON_DATA += json.dumps(scss_group)  # add the scss groups to the json data
+
+        for scss_class in Design.get_registered_classes():
+            self.__JSON_DATA += json.dumps(scss_class)  # add the scss class to the json data
+
+        return self
+
+    def make_app_component_data(self, components_data: str) -> Self:
         """
         This method is used to create the app-component data.
         ### Arguments
@@ -25,4 +33,14 @@ class DataBuilder:
         ### Returns
         value: None
         """
-        pass
+
+        self.__JSON_DATA += json.dumps(components_data)  # add the component_data to the json data based on the app render
+
+        return self
+
+    def send(self):
+        """
+        Send the json data to the frontend
+        """
+
+        return self
