6b49b8d267094ac53b0a5b88e67888f7c5ccf1f3
Hadházy Tamás <helsgithub15@gmail.com>
2022-12-04 12:08:29 +0100

    Refactor data builder

--- slobypy/communication/data_builder.py
+++ slobypy/communication/data_builder.py
@@ -15,18 +15,15 @@ class DataBuilder:
     This class is build the data for the frontend.
     """
     # Todo: Type hint here
-    __JSON_DATA = ""  # pylint: disable=invalid-name
+    __JSON_DATA: str = ""  # pylint: disable=invalid-name
 
     def make_scss_data(self) -> Self:
         """
         This method is used to create the scss data.
         """
 
-        for scss_group in Design.get_registered_groups():
-            self.__JSON_DATA += json.dumps(scss_group)  # add the scss groups to the json data
-
-        for scss_class in Design.get_registered_classes():
-            self.__JSON_DATA += json.dumps(scss_class)  # add the scss class to the json data
+        for scss_data in Design.USED_CLASSES:
+            self.__JSON_DATA += json.dumps(scss_data)  # add the scss data to the json.
 
         return self
 
@@ -44,9 +41,9 @@ class DataBuilder:
 
         return self
 
-    def send(self):
+    def get_json(self) -> str:
         """
         Send the json data to the frontend
         """
 
-        return self
+        return self.__JSON_DATA
