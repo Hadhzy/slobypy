abb812dcd0f1706a295902d3aac362badf355376
Hadházy Tamás <helsgithub15@gmail.com>
2022-12-26 09:31:19 +0100

    Improve ``router`` with ``dynamic routes`` and ``__iter__``.

--- slobypy/react/router.py
+++ slobypy/react/router.py
@@ -1,22 +1,41 @@
 from __future__ import annotations
-from typing import Generator
+from typing import Generator, Self
 
 
-#Todo: Add dynamic feature
 class SloRouter:
     """Used to define the route"""
-    def __init__(self, route: str) -> None:
-            self.route = route
 
-    def __truediv__(self, other: str) -> None:
+    def __init__(self, curr_route: str) -> None:
+            self.route = curr_route
+            self._dynamic_routes: list[dict] = []
+            self._check_dynamic_routes()
 
+    def __truediv__(self, other: str) -> Self:
+        """Add an endpoint and return a brand new SloRouter"""
         if self.route.endswith("/"):
             self.route += other
+
+            return self.__get_object()
+
         else:
             self.route += "/" + other
 
+            return self.__get_object()
+
+    def __get_object(self) -> Self:
+        """Used to return a SloRouter with the latest route"""
+        obj = SloRouter(self.route)
+        return obj
+
     def __str__(self) -> str:
         return self.route
+
+    def _check_dynamic_routes(self):
+        endpoints_as_list = self._endpoints_as_list()
+
+        for item in endpoints_as_list:
+            if item.startswith(":"):
+                self._dynamic_routes.append({item: endpoints_as_list.index(item)})
 
     def endpoints_count(self) -> int:
         """Used to return the endpoints in an uri"""
@@ -29,6 +48,9 @@ class SloRouter:
                 endpoints_count += 1
 
         return endpoints_count
+
+    def dynamic_routes_iter(self) -> list:
+        return self._dynamic_routes
 
     def _endpoints_as_list(self) -> list[str]:
         return self.route.split("/")
