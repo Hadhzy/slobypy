c252ca8dee232f32b0f326f895ab6a390be5bde5
Hadházy Tamás <helsgithub15@gmail.com>
2023-01-26 22:34:28 +0100

    ``Added Component based-view""

---
+++ examples/component_based_view.py
@@ -0,0 +1,42 @@
+from slobypy import *
+
+
+@SlApp.component("/route1")
+class MyComponent1(Component):
+    @property
+    def name(self):
+        return "MyComponent1"
+
+    def on_click(self):
+        print(self.name, "has been clicked!")
+
+    def body(self):
+        yield P("test1", onClick=self.on_click)
+        yield MyComponent2(props={"important_data": "Woah, this is a hahao10!"})
+
+
+@SlApp.component("/route3")
+class MyComponent3(Component):
+    @property
+    def name(self):
+        return "MyComponent1"
+
+    def on_click(self):
+        print(self.name, "has been clicked!")
+
+    def body(self):
+        yield P("test", onClick=self.on_click)
+
+
+
+class MyComponent2(Component):
+    @property
+    def name(self):
+        return "MyComponent2"
+
+    def on_click(self):
+        print(self.name, "has been clicked!")
+
+    def body(self):
+        yield P(self.props["important_data"], onClick=self.on_click)
+
