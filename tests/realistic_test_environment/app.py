ea8d4b62d9a37df9466ae7677a98a2a7331e29e3
Coddo <40121850+Coddo-Python@users.noreply.github.com>
2023-01-28 23:39:45 +0400

    Update `Context` to use `__call__` (#12)

--- tests/realistic_test_environment/app.py
+++ tests/realistic_test_environment/app.py
@@ -2,15 +2,10 @@ from tests.realistic_test_environment.components.example_component import MyComp
 from slobypy.react.component import *
 from slobypy.react.context import Context
 
-
-class MyContext(Context):
-    def create_data(self):
-        yield {"test": 1}
-
-
 class MyApp(AppComponent):
     def body(self):
-        yield MyContext(MyComponent3())
+        myContext = Context("Hello World")
+        yield myContext(MyComponent3())
 
 
 MyApp()
