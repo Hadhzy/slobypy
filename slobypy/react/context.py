48705d55d3079cb50175fab063473770f1870d01
Coddo-Python <coddobusiness@gmail.com>
2023-01-28 23:32:20 +0400

    Update `Context` to use `__call__`

--- slobypy/react/context.py
+++ slobypy/react/context.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 # Built-in
-from typing import Generator, Any
+from typing import Generator, Any, Self
 
 # This project
 import slobypy.react.component as component_file
 from slobypy.errors.react_errors import NotValidComponent
 
 from .tools import *
+
 __all__ = (
     "Context",
 )
@@ -16,7 +17,11 @@ class Context:
     """
     Used to provide data transfer for the user.
     """
-    def __init__(self, *components: "component_file.Component") -> None:
+
+    def __init__(self, starting_data) -> None:
+        self.data = starting_data
+
+    def __call__(self, *components: "component_file.Component") -> Self:
         """
         ### Arguments
          - components: Slobypy Components as an object -> Component().
@@ -26,11 +31,7 @@ class Context:
 
         self.components = components  # Defined components by the user
         self.components_data: list = []  # Used to store the checked components
-
-    # noinspection PyMethodMayBeStatic
-    def create_data(self) -> Generator[Any, None, None]:  # type: ignore  # this comment should be deleted when the method will be implemented
-        """Used to create the data by yielding it"""
-        pass
+        return self
 
     def run(self) -> None:
         """Used to render the components inside the context"""
@@ -40,12 +41,11 @@ class Context:
 
             self.components_data.append(component)  # add the component to the components_data after check
 
-            for context_data in self.create_data():  # loop through the create_data in order to get the context_data
+            if component_dict := find_component_in_app(
+                    component):  # Return the current dict(based on the component)
+                component_dict["context"] = self.data  # Add the context_data to the specific dict
 
-                if component_dict := find_component_in_app(component):  # Return the current dict(based on the component)
-                    component_dict["context"] = context_data  # Add the context_data to the specific dict
-
-    #noinspection PyMethodMayBeStatic
+    # noinspection PyMethodMayBeStatic
     def _check_component_type(self, component) -> None:
         if not isinstance(component, component_file.Component):
             raise NotValidComponent(f"{component} is not valid!")
